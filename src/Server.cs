3e04614ef7dff496a09df2055b8fc4293a47ff4d
Loïc Bouvier <loic.bouvier80@gmail.com>
2025-07-02 11:11:58 +0200

    Respond to multiple PINGs

--- src/Server.cs
+++ src/Server.cs
@@ -8,6 +8,26 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
-var client = server.AcceptSocket(); // wait for client
-client.Send(Encoding.UTF8.GetBytes("+PONG\r\n"));
-client.Disconnect(true);
+
+var socket = server.AcceptSocket(); // wait for client
+while (true)
+{
+    byte[] buffer = new byte[1024];
+    int bytesReceived = socket.Receive(buffer);
+
+    if (bytesReceived == 0)
+        break;
+    
+    socket.Send(Encoding.UTF8.GetBytes("+PONG\r\n"));
+}
+
+try
+{
+    socket.Shutdown(SocketShutdown.Both);
+}
+catch (SocketException) 
+{
+    // Ignore if already closed
+}
+
+socket.Close();
